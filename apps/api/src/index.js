90c2e88997a22699481ac47c853e6f005a99f904
Vikhyath S <49331452+vikhyathdevadiga@users.noreply.github.com>
2025-10-06 10:54:03 +0530

    Update index.js

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -3,7 +3,7 @@ import {log} from "@repo/logger";
 const app = new Hono();
 
 app.get("/", (c) => {
-  log("Hello from the Hono API!");
+  console.error("Hello from the Hono API!");
   return c.text("Hello from the Hono API!");
 });
 
